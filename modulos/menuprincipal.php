4f25431939275d7ef00685478b62cc74016a8c8b
Frontend Update <arte@update.pe>
2018-01-31 17:08:54 -0500

    luis

--- modulos/menuprincipal.php
+++ modulos/menuprincipal.php
@@ -30,11 +30,12 @@
     </a>
     <ul>
           <li><a href="http://old.update.pe/" class="btn-floating colormenu6 tooltipped" data-position="left" data-delay="50" data-tooltip="Version Anterior"><img src="  img/hombre-mayor.png" style="display: inline-block; margin: 5px -2px 0 0;"></a></li>
-          <li><a href="landin.html" class="btn-floating colormenu6 tooltipped button-collapse" data-position="left"  data-delay="50" data-tooltip="Contacto" data-activates="slide-out"><i class="material-icons">perm_phone_msg</i></a></li>      
-          <li><a href="landin.html" class="btn-floating colormenu5 tooltipped" data-position="left" data-delay="50" data-tooltip="Marketing Digital">  <i class="material-icons">timeline</i></a></li>
-          <li><a href="landin.html" class="btn-floating colormenu4 tooltipped" data-position="left" data-delay="50" data-tooltip="Plataformas a Medida">   <i class="material-icons">developer_mode</i></a></li>
-          <li><a href="landin.html" class="btn-floating colormenu3 tooltipped" data-position="left" data-delay="50" data-tooltip="Diseño paginas Web">  <i class="material-icons">desktop_mac</i></a></li>
-          <li><a href="landin.html" class="btn-floating colormenu2 tooltipped" data-position="left" data-delay="50" data-tooltip="Diseño de Marcas" href=""><i class="material-icons">brush</i></a></li>
+   
+          <li><a href="landin.html" class="btn-floating colormenu6 tooltipped" data-position="left" data-delay="50" data-tooltip="Marketing Digital">  <i class="material-icons">timeline</i></a></li>
+          <li><a href="landin.html" class="btn-floating colormenu5 tooltipped" data-position="left" data-delay="50" data-tooltip="Plataformas a Medida">   <i class="material-icons">developer_mode</i></a></li>
+          <li><a href="landin.html" class="btn-floating colormenu4 tooltipped" data-position="left" data-delay="50" data-tooltip="Diseño paginas Web">  <i class="material-icons">desktop_mac</i></a></li>
+          <li><a href="landin.html" class="btn-floating colormenu3 tooltipped" data-position="left" data-delay="50" data-tooltip="Diseño de Marcas" href=""><i class="material-icons">brush</i></a></li>
+          <li><a href="landin.html" class="btn-floating colormenu2 tooltipped button-collapse" data-position="left"  data-delay="50" data-tooltip="Contacto" data-activates="slide-out"><i class="material-icons">perm_phone_msg</i></a></li>   
     </ul>
   </div>
 <!--botonndownrigth-->
