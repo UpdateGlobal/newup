c6c86c53d1fb180cbde29c6c08687ad0405dc0de
Frontend Update <arte@update.pe>
2018-02-08 15:09:05 -0500

    8 febrero

---
+++ blog/index.php
@@ -0,0 +1,224 @@
+  <!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Titulo del Sitio</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="DESCRIPCION DEL SITIO" />
+      <meta name="keywords" content="PALABRAS CLAVER" />
+      <meta name="author" content="luis alberto bernal fuentes" />
+
+        <!-- Facebook and Twitter integration -->
+      <meta property="og:title" content=""/>
+      <meta property="og:image" content=""/>  
+      <meta property="og:url" content=""/>
+      <meta property="og:site_name" content=""/>
+      <meta property="og:description" content=""/>
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:url" content="" />
+
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" type="text/css" href="css/animate.css">
+      <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+      <link rel="stylesheet" type="text/css" href="css/materialize.css">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+
+      <!--normalizecss-->
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+          <!--contenidoBODY-->
+
+    <div class="parallax-container">
+      <div class="parallax puntoblog center">
+        <h1 class="thin white-text bannerblog">Nuestro Blog</h1>
+        <img src="img/base-icon.jpg" >
+             <h1 class="thin white-text">Nuestro Blog</h1>
+      </div>
+    </div>
+
+
+
+
+
+  <div class="parallax-container">
+    <div class="parallax"><img src="images/parallax1.jpg"></div>
+  </div>
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header">Parallax</h2>
+      <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+    </div>
+  </div>
+  <div class="parallax-container">
+    <div class="parallax"><img src="images/parallax2.jpg"></div>
+  </div>
+
+<div class="container">
+    <div class="row">
+      <div class="col s12 m3">
+
+        <h1 class="regular">Categorias</h1>
+        <ul>
+          <li class="thin spacelist">Categoria</li>
+          <li class="thin spacelist">Categoria</li>
+          <li class="thin spacelist">Categoria</li>
+          <li class="thin spacelist">Categoria</li>
+          <li class="thin spacelist">Categoria</li>
+        </ul>
+
+        <div class="divider"></div>
+        <h1 class="regular">Tags</h1>
+        <div class="chip">Webs</div>
+        <div class="chip">Plataformas</div>
+        <div class="chip">Redes sociales</div>
+        <div class="chip">Frontend</div>
+        <div class="chip">Backend</div>
+        <div class="chip">Diseño</div>
+        <div class="chip">Luis</div>
+        <br><br>
+
+        <div class="divider"></div>         
+        <h1 class="regular">Nuevos Post</h1>
+        <ul style=" display: flex;">
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li>
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li>
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li><br>
+        </ul>
+        <ul style=" display: flex;">
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li>
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li>
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li><br>
+        </ul>
+        <ul style=" display: flex;">
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li>
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li>
+          <li><a href="#"><img src="http://via.placeholder.com/60x60" class="piclist responsive-img"></a></li><br>
+        </ul>
+
+      </div>
+
+
+
+       <div class="col s12 m9">
+
+
+
+              <div class="row">
+                <div class="col s12 m6">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="http://via.placeholder.com/400x300">
+                      <span class="card-title">Titulo</span>
+                      <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat.</p>
+                    </div>
+                  </div>
+                </div>
+            <br><br><br>
+                  <div class="col s12 m6">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="http://via.placeholder.com/400x300">
+                      <span class="card-title">Titulo</span>
+                      <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>   
+
+              <div class="row">
+                <div class="col s12 m6" style="">
+            <br><br><br>
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="http://via.placeholder.com/400x300">
+                      <span class="card-title">Titulo</span>
+                      <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat.</p>
+                    </div>
+                  </div>
+                </div>
+
+                  <div class="col s12 m6" >
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="http://via.placeholder.com/400x300">
+                      <span class="card-title">Titulo</span>
+                      <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+       </div>
+    </div> 
+        <div align="center">
+            <ul class="pagination">
+              <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+              <li class="active"><a href="#!">1</a></li>
+              <li class="waves-effect"><a href="#!">2</a></li>
+              <li class="waves-effect"><a href="#!">3</a></li>
+              <li class="waves-effect"><a href="#!">4</a></li>
+              <li class="waves-effect"><a href="#!">5</a></li>
+              <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
+        </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+          <!--contenidoBODY-->
+                <!--Import jQuery before materialize.js-->
+                <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+                <script type="text/javascript" src="js/materialize.min.js"></script>
+                  <script type="text/javascript" src="js/materialize.js"></script>
+
+                <script type="text/javascript">
+                $(document).ready(function() {
+                  $('.parallax').parallax();
+  
+
+                });
+                </script>
+    </body>
+  </html>
--- webs/index.php
+++ webs/index.php
@@ -6,12 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="keywords" content="">
-    <title>Titulo</title>
+    <title>Diseños Webs</title>
     <link rel="stylesheet" href="assets/style.css">
     <script src="https://use.fontawesome.com/9576790a31.js"></script>
     <link rel="stylesheet" type="text/css" href="assets/animate.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed" rel="stylesheet">
@@ -24,47 +23,44 @@
 
 <body class="my-body" data-spy="scroll" data-target="#main-navbar" data-offset="70">
 
-
-
 <div class="headere">
   <div class="progress-container">
     <div class="progress-bar" id="myBar"></div>
   </div>  
 </div>
 
-<div class="row">
-    <img src="img/log1.png" alt="" class="fiximg">
-</div>
+    <div class="row">
+        <img src="img/log1.png" alt="" class="fiximg">
+    </div>
 
-<?php
+    <?php
+    include('../modulos/menuprincipal.php');
+    ?>
 
-include('../modulos/menuprincipal.php');
-?>
-
-<div class="content">
-  <video id="player" src="video-mano.mp4" autoplay loop muted></video>
-</div>
-
-
-<!-- <video autoplay="autoplay" loop="loop" id="video_background" preload="auto" volume="50" class="responsive-video adaptvideo" />
-    <source src="video-mano.mp4" type="video/mp4" />
-</video/> -->
  
-        <div class=""> 
-            <div class="container">
-                <br><br>
-            <h1 class="thin">Nuestro Diseños Webs</h1> 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+<div class="content">
+      <video id="player" src="video-mano.mp4" autoplay loop muted></video>
+    </div>
+
+      
+
+
+
+        <div class="container">
+                <br><br><br><br><br><br><br><br><br><br><br>
+                <h1 class="thin">Nuestro Diseños Webs</h1> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                </div>
             <section id="horizontal" class="">
                 <div class="jumbotron text-left" style="background: url('img/web2.png') repeat center;" data-paroller-factor="2" data-paroller-direction="horizontal">
                     <div class="card-block addcard container" align="left">
-                        <h1 class="py-5 text-white thin">http://frelatos.pe/</h1>
+                        <h1 class="py-5 text-white thin">frelatos.pe/</h1>
                         <p class="text-white thin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -84,6 +80,9 @@ include('../modulos/menuprincipal.php');
                         <div class="chip thin">Frontend</div> 
                     </div>
                 </div>
+
+
+
                 <div class="jumbotron text-center" style="background: url('img/web1.png') center;" data-paroller-factor="-2" data-paroller-direction="horizontal">
                     <div class="card-block addcard container" align="left">
                         <h1 class="py-5 text-white thin">Naricitasplaza.pe</h1>
@@ -107,7 +106,6 @@ include('../modulos/menuprincipal.php');
                         <br><br>
                     </div>
                 </div>
-
                 <div class="jumbotron text-center" style="background: url('img/web6.jpg') center;" data-paroller-factor="2" data-paroller-direction="horizontal">
                     <div class="card-block addcard container" align="left">
                         <h1 class="py-5 text-white thin">Isotech.pe</h1>
@@ -179,7 +177,7 @@ include('../modulos/menuprincipal.php');
                 </div>
                 <div class="jumbotron text-center" style="background: url('img/web7.jpg') center; background-size: cover;" data-paroller-factor="-2" data-paroller-direction="horizontal">
                     <div class="card-block addcard container" align="left">
-                        <h1 class="py-5 text-white thin">http://www.integro365.com/</h1>
+                        <h1 class="py-5 text-white thin">integro365.com/</h1>
                         <p class="text-white thin">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -200,7 +198,6 @@ include('../modulos/menuprincipal.php');
                         <br><br>
                     </div>
                 </div>
-              
             </section>  
         </div>    
 
@@ -208,6 +205,12 @@ include('../modulos/menuprincipal.php');
 
 include('../modulos/footer.php');
 ?>
+
+
+
+ <!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
 
 
 
@@ -221,28 +224,33 @@ include('../modulos/footer.php');
 <!--Paroller-->
 <script src="dist/jquery.paroller.js"></script>
 <script type="text/javascript" src="js/js.js"></script>
-<script>
-    $(function () {
-        $('[data-paroller-factor]').paroller();
-        $('.paroller').paroller({
-            factor: 0.4,
-            type: 'foreground'
-        });
-        $('body').scrollspy({target: '#main-navbar'});
 
-    });
-</script>
-<script>
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-</script>
+
+
+                <script>
+                    $(function () {
+                        $('[data-paroller-factor]').paroller();
+                        $('.paroller').paroller({
+                            factor: 0.4,
+                            type: 'foreground'
+                        });
+                        $('body').scrollspy({target: '#main-navbar'});
+
+                    });
+                </script>
+                <script>
+                    // When the user scrolls the page, execute myFunction 
+                    window.onscroll = function() {myFunction()};
+
+                    function myFunction() {
+                      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+                      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+                      var scrolled = (winScroll / height) * 100;
+                      document.getElementById("myBar").style.width = scrolled + "%";
+                    }
+                </script>
+
 
 </body>
 </html>
