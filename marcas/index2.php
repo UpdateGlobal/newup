0c764e6b4cefb2d88385cb7fb222bf50d86571ed
Frontend Update <arte@update.pe>
2018-02-27 17:09:05 -0500

    luis-20180227

---
+++ marcas/index2.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html lang="es">
+<head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>New Update</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="DESCRIPCION DEL SITIO" />
+      <meta name="keywords" content="PALABRAS CLAVER" />
+      <meta name="author" content="luis alberto bernal fuentes" />
+        <!-- Facebook and Twitter integration -->
+      <meta property="og:title" content=""/>
+      <meta property="og:image" content=""/>
+      <meta property="og:url" content=""/>
+      <meta property="og:site_name" content=""/>
+      <meta property="og:description" content=""/>
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:url" content="" />
+
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/> 
+      <link rel="stylesheet" type="text/css" href="css/animate.css">  
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed" rel="stylesheet">
+      <script src="https://use.fontawesome.com/3c86cf9c2d.js"></script>
+        <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="css/responsivemarcas.css">
+</head>
+<body>
+<?php
+include('../modulos/menuprincipal.php');
+?>
+
+
+<div style="border-bottom: 1.5px solid #9e9e9e ;">
+  <div style="margin: 0px 200px 0px; width: auto; ">
+    <div class="ba-slider">
+      <img src="img/11.png" alt="">       
+      <div class="resize">
+        <img src="img/12.png" alt="" style="margin: 0px 200px 0px; margin-left: 0px; width: auto;">
+      </div>
+      <span class="handle"></span>
+    </div>
+  </div>
+</div>
+
+<div style="background-color: #53bcbd; margin-top: 0px;" align="center">
+  <div class="row" style="padding: 100px 100px 100px;">
+  <h1 class="uptext">Desarrollo de <span class="gris">Marcas Exitantes</span> </h1>
+  <h1 class="updateregular">De  mas mejor calidad.</h1>
+  </div>
+</div>
+<br><br><br>
+
+<div class="container dir">
+       <h1 class="rotar">Marcas</h1>
+ <div class="row" >
+      <div class="col s12" align="left">
+
+
+
+
+
+<div class="row" style="display: inline-block;">
+      <div class="col s4 offset-s2 c-test js-tilt responsive-img"  style="background-image: url(img/sanus.png); background-repeat: no-repeat; margin-top: -4%;"></div>
+      <div class="col s6 thin">
+            <h1 class="uptext" style=" color: #50bdbe;">Descripcion</h1>
+            <h3 class="updateregular black-text">Empresa</h3>
+            <span class="updateregular black-text" style="font-size: 12px;">Conceptualizacion</span>
+            <p class="updateregular black-text" style="font-size: 0.8em;">The first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the images.The first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the imagesThe first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the images</p>
+      </div>
+</div>
+<div class="col s6 offset-s6">
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modal123">Lista</a>
+
+              <!-- Modal Structure -->
+              <div id="modal123" class="modal">
+                <div class="modal-content carousel" controls>
+                            <a class="carousel-item" href="#one!"><img src="img/sanus.png" width="320" height="220"></a>
+                            <a class="carousel-item" href="#two!"><img src="img/frelatos.png" width="320" height="220"></a>
+                            <a class="carousel-item" href="#three!"><img src="img/iso.png" width="320" height="220"></a>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Salir</a>
+                </div>
+              </div>
+
+
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modal">Modal</a>
+
+              <!-- Modal Structure -->
+              <div id="modal" class="modal">
+                <div class="modal-content">
+                  <h4>Modal Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modals">Modal</a>
+
+              <!-- Modal Structure -->
+              <div id="modals" class="modal">
+                <div class="modal-content">
+                  <h4>Modals Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+</div><!--modales-->
+
+
+<div class="hrtes"></div>
+
+
+
+
+<div class="row" style="display: inline-block;">
+      <div class="col s4 offset-s2 c-test js-tilt responsive-img"  style="background-image: url(img/frelatos.png); background-repeat: no-repeat; margin-top: -4%;"></div>
+      <div class="col s6 thin">
+            <h1 class="uptext" style=" color: #50bdbe;">Descripcion</h1>
+            <h3 class="updateregular black-text">Empresa</h3>
+            <span class="updateregular black-text" style="font-size: 12px;">Conceptualizacion</span>
+            <p class="updateregular black-text" style="font-size: 0.8em;">The first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the images.The first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the imagesThe first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the images</p>
+      </div>
+</div>
+<div class="col s6 offset-s6">
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modal1">Modal1</a>
+
+              <!-- Modal Structure -->
+              <div id="modal1" class="modal">
+                <div class="modal-content">
+                  <h4>Modal1 Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+
+
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modal">Modal</a>
+
+              <!-- Modal Structure -->
+              <div id="modal" class="modal">
+                <div class="modal-content">
+                  <h4>Modal Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modals">Modal</a>
+
+              <!-- Modal Structure -->
+              <div id="modals" class="modal">
+                <div class="modal-content">
+                  <h4>Modals Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+</div><!--modales-->
+
+
+
+
+<div class="hrtes"></div>
+
+
+
+<div class="row" style="display: inline-block;">
+      <div class="col s4 offset-s2 c-test js-tilt responsive-img"  style="background-image: url(img/iso.png); background-repeat: no-repeat; margin-top: -4%;"></div>
+      <div class="col s6 thin">
+            <h1 class="uptext" style=" color: #50bdbe;">Descripcion</h1>
+            <h3 class="updateregular black-text">Empresa</h3>
+            <span class="updateregular black-text" style="font-size: 12px;">Conceptualizacion</span>
+            <p class="updateregular black-text" style="font-size: 0.8em;">The first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the images.The first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the imagesThe first CSS block is similar to the code in Example 1. In addition, we have added what should happen when a user hovers over one of the images</p>
+      </div>
+</div>
+<div class="col s6 offset-s6">
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modal1">Modal1</a>
+
+              <!-- Modal Structure -->
+              <div id="modal1" class="modal">
+                <div class="modal-content">
+                  <h4>Modal1 Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+
+
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modal">Modal</a>
+
+              <!-- Modal Structure -->
+              <div id="modal" class="modal">
+                <div class="modal-content">
+                  <h4>Modal Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+
+              <!-- Modal Trigger -->
+              <a class="waves-effect waves-light chip modal-trigger" href="#modals">Modal</a>
+
+              <!-- Modal Structure -->
+              <div id="modals" class="modal">
+                <div class="modal-content">
+                  <h4>Modals Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+              </div>
+</div><!--modales-->
+
+
+
+
+
+
+                          
+             
+      </div><!--cols12-->
+ </div><!--row-->
+</div><!--conatiner dir-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+<script type="text/javascript" src="js/vaiven.js"></script>
+<script type="text/javascript" src="js/vaiven2.js"></script>
+<script  src="js/index.js"></script><!--slider-->
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+        $('.carousel').carousel();
+        $('.materialboxed').materialbox();
+        $('.modal').modal();
+        $('select').material_select();
+        $('.slider').slider({
+          indicators: false,
+          height: 600
+        });
+        $(".button-collapse").sideNav();
+
+      });
+  </script>
+ </body>
+ </html>
