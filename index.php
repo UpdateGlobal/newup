989d0e37540d12dcb5fd05ed21defb6542027b41
luizink <luizupdate@gmail.com>
2018-04-19 17:56:07 -0500

    chat

--- index.php
+++ index.php
@@ -109,11 +109,11 @@
         <div class="row" align="left">
           <div class="col m12 s12">
             
-              <!-- BEGIN JIVOSITE CODE {literal} -->
-                <script type='text/javascript'>
-                (function(){ var widget_id = '5zfgH3KqVd';var d=document;var w=window;function l(){
-                var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
-              <!-- {/literal} END JIVOSITE CODE -->
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'ori7edOZn5';var d=document;var w=window;function l(){
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
 
           </div>
         </div>
