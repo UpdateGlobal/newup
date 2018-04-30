2d4f3d58a6ff747c973ec468c03c4ba712bef0dd
Frontend Update <arte@update.pe>
2018-04-30 11:31:39 -0500

    luis

---
+++ landing/index.php
@@ -0,0 +1,257 @@
+<!doctype html>
+<html lang="es">
+<head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Update - Desarrollo de Marca</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel=”canonical” href=”https://update.pe/marcas>
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+      <meta name="description" content="Somos una Agencia de Marketing en Perú, especializada en desarrollo de marca, Diseño y Desarrollo web, Estrategias de Marketing tradicional y Marketing Digital con visión global, anteponemos tus objetivos comerciales sin dejar el lado creativo." />
+      <meta name="keywords" content="marketing, marketing digital, diseño web, diseño grafico, desarrollo de marca, creación de logotipos, paginas webs, branding" />
+      <meta name="author" content="luis alberto bernal fuentes" />
+        <!-- Facebook and Twitter integration -->
+      <meta property="og:title" content=""/>
+      <meta property="og:image" content=""/>
+      <meta property="og:url" content=""/>
+      <meta property="og:site_name" content=""/>
+      <meta property="og:description" content=""/>
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:url" content="" /> <link rel="stylesheet" type="text/css" href="css/materialize.css" /> 
+      <link rel="stylesheet" type="text/css" href="css/animate.css">  
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700">
+      <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed">
+      <link rel="stylesheet" type="text/css" href="css/responsivemarcas.css">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+      <script src="https://use.fontawesome.com/3c86cf9c2d.js"></script>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!-- Global site tag (gtag.js) - Google Pichuytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-74004200-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-74004200-1');
+        </script>
+       <!-- Global site tag (gtag.js) - Google Pichuytics -->
+      <!--maquina de escribir-->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      
+
+</head>
+<body>
+
+
+
+<div class="navbar-fixed">
+  <nav>
+    <div class="nav-wrapper" style="color: white;">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li class="item_menu">Sass</li>
+        <li class="item_menu">Components</li>
+        <li class="item_menu">JavaScript</li>
+      </ul>
+    </div>
+  </nav>
+</div>       
+
+        
+
+
+
+
+    <!--slider-->
+      <div class="slider">
+        <ul class="slides">
+          <li>
+            <img src="images/993@2x.jpg"> <!-- random image -->
+            <div class="caption" style="padding: 50px;">
+              <h3 class="sd_ti">Esto es sólo “para empresarios soñadores”</h3>
+              <h5 class="sd_suti">NO TE QUEDES EN LO MISMO</h5>
+            </div>
+          </li>
+         <!--  <li>
+            <img src="images/993.jpg"> 
+            <div class="caption left-align">
+              <h3>Left Aligned Caption</h3>
+              <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+          </li>
+          <li>
+            <img src="images/993.jpg"> 
+            <div class="caption right-align">
+              <h3>Right Aligned Caption</h3>
+              <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+          </li> -->
+        </ul>
+      </div>
+    <!--slider-->
+    <!--efectmachiwrite-->
+    <div style="background-color: #53bcbd; margin-top: 0px;" align="center">
+      <div class="row" style="padding: 30px 30px 30px; margin-bottom: 0px;">
+      <h1 class="uptext">Desarrollamos <span class="gris" id="typed"></span></h1>
+      <h1 class="updateregular">Con los más altos estandares</h1> 
+            <script type="text/javascript">
+              $(function(){
+                $("#typed").typed({
+                  strings: ["Marcas Poderosas", "Conceptos Fuertes", "Marcas que enganchan", "Ideas que venden"],
+                  typeSpeed: 30,
+                  backDelay: 1500,
+                  loop: true,
+                  loopCount: false,
+                  callback: function(){ foo(); }
+                });
+                function foo(){ console.log("Callback"); }
+              });
+            </script>
+      </div>
+    </div>
+    <!--efectmachiwrite-->
+
+
+<div class="container-fluid" align="center">
+  <div class="row" style="margin: 0px;">
+      <h5 class="sd_suti" style="margin: 70px;">Lo que <span class="verde">necesitas saber</span> </h5>
+  </div>
+</div>
+
+    <!--infoItem-->
+    <div class="container-fluid info_txt bg_info" align="center">
+        <div class="row" style="margin: 0;">
+          <div class="col s12 m6 l6 bg_1" style="padding: 50px 150px 50px;"> 
+            <span><img src="img/person.png" class="item_img" ></span>
+            <h3 class="item_info uptext">¿Qué conseguiré desarrollando mi marca con Update ?</h3>
+            <hr class="hr">
+            <p class="uptext_icon">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div class="col s12 m6 l6 bg_2" style="padding: 50px 150px 50px;"> 
+            <span><img src="img/pincel.png" class="item_img" ></span>
+            <h3 class="item_info uptext">¿Qué necesito para empezar con el servicio de desarrollo de marca?</h3>
+            <hr class="hr">
+            <p class="uptext_icon">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+        <div class="row" style="margin: 0;">
+          <div class="col s12 m6 l6 bg_2" style="padding: 50px 150px 50px;"> 
+            <span><img src="img/person.png" class="item_img" ></span>
+            <h3 class="item_info uptext">¿Con el servicio puedo obtener una página web para mi marca?</h3>
+            <hr class="hr">
+            <p class="uptext_icon">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div class="col s12 m6 l6 bg_1" style="padding: 50px 150px 50px;"> 
+            <span><img src="img/pincel.png" class="item_img" ></span>
+            <h3 class="item_info uptext">¿Cuanto cuesta el servicio desarrollo de Marca?</h3>
+            <hr class="hr">
+            <p class="uptext_icon">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+    </div>
+    <!--infoItem-->
+
+ 
+
+
+
+  <ul class="collapsible">
+    <li>
+      <div class="container-fluid bg_grey collapsible-header">
+        <div class="row" style="margin-bottom: 0px;">
+          <div class="col s12 m12">
+            <h5 class="sd_suti_info">¿Qué incluye el servicio <span class="verde">de Desarrollo de marca? <br> Para Da </span> le click <a href="#!">AQUI</a> </h5>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
+
+
+
+
+
+
+
+
+
+<!--   <ul class="collapsible">
+    <li>
+      <div class="collapsible-header">First</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
+
+
+
+
+
+
+
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!--zpinChat-->
+
+      <!--Start of Zendesk Chat Script-->
+      <script type="text/javascript">
+      window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+      d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+      _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+      $.src="https://v2.zopim.com/?5eb2dhLOYkK9hUr7TrwizgJSyPxysQlv";z.t=+new Date;$.
+      type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+      </script>
+      <!--End of Zendesk Chat Script-->
+
+    <!--zpinChat-->
+
+
+    <?php
+    include('../modulos/footer.php');
+    ?>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    <script src="js/index.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+          $('.scrollspy').scrollSpy();
+          $('.carousel').carousel();
+          $('.materialboxed').materialbox();
+          $('.modal').modal();
+          $('select').material_select();
+          $('.slider').slider({
+            indicators: false,
+            height: 1000
+          });
+          $(".button-collapse").sideNav();
+
+        });
+
+    </script>
+ </body>
+ </html>
