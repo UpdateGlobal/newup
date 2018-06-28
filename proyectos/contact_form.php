f33ccd1150e91082f24fb636a081612d988942c4
backendupdate <desarrolloweb@update.pe>
2018-06-28 09:34:52 -0500

    Estilos

--- proyectos/contact_form.php
+++ proyectos/contact_form.php
@@ -1,14 +1,20 @@
+<?php include("../cms/module/conexion.php"); ?>
 <?php
 $toEmail = "luizupdate@gmail.com, updatechristian@gmail.com";
 $subject = "Mensaje enviado desde Marcas UPDATE";
 $mailHeaders = "From: " . $_POST["nombre"] . "<". $_POST["mail"] .">\r\n";
 
-$mensaje .= "Informacion del Contacto\n";
+$nombres = mysqli_real_escape_string($enlaces, $_POST["nombre"]);
+$email = mysqli_real_escape_string($enlaces, $_POST["mail"]);
+$telefono = mysqli_real_escape_string($enlaces, $_POST["telefono"]);
+$comentario = mysqli_real_escape_string($enlaces, $_POST["mensaje"]);
+
+$mensaje .= "Información del Contacto\n";
 $mensaje .= "------------------------\n";
-$mensaje .= "Nombres		:".$_POST["nombre"]."\n";
-$mensaje .= "Email			:".$_POST["mail"]."\n";
-$mensaje .= "Telefono		:".$_POST["telefono"]."\n";
-$mensaje .= "Mensaje		:".$_POST["mensaje"]."\n";
+$mensaje .= "Nombres : ".$nombres."\n";
+$mensaje .= "Email : ".$email."\n";
+$mensaje .= "Telefono : ".$telefono."\n";
+$mensaje .= "Mensaje : ".$comentario."\n";
 
 if(mail($toEmail, $subject, $mensaje, $mailHeaders)) {
 	print "<div class='card-panel lighten-2' style='background: #ff0000; padding: 14px; color: #fff; font-size: 16px; font-weight: 700;'>Gracias, su mensaje fue enviado exitosamente.</div>";
