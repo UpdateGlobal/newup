654eac237333ab1e1eccb903d3d8b1c4c1386af0
Frontend Update <arte@update.pe>
2018-07-06 15:29:24 -0500

    add galery web

---
+++ webs/home.php
@@ -0,0 +1,455 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Update - Desarrollo de Webs</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel=”canonical” href=”https://update.pe/webs>
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+      <meta name="description" content="Somos una Agencia de Marketing en Perú, especializada en desarrollo de marca, Diseño y Desarrollo web, Estrategias de Marketing tradicional y Marketing Digital con visión global, anteponemos tus objetivos comerciales sin dejar el lado creativo." />
+      <meta name="keywords" content="marketing, marketing digital, diseño web, diseño grafico, desarrollo de marca, creación de logotipos, paginas webs, branding" />
+      <meta name="author" content="luis alberto bernal fuentes" />
+      <!-- Facebook and Twitter integration -->
+      <meta property="og:title" content=""/>
+      <meta property="og:image" content=""/>
+      <meta property="og:url" content=""/>
+      <meta property="og:site_name" content=""/>
+      <meta property="og:description" content=""/>
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:url" content="" />
+
+
+      <link rel="stylesheet" href="assets/style.css">
+      <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+      <link rel="stylesheet" type="text/css" href="assets/animate.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed" rel="stylesheet">
+      <script src="https://use.fontawesome.com/3c86cf9c2d.js"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="assets/font-awesome.css">
+      <link rel="stylesheet" type="text/css" href="assets/materialize.css">
+      <link rel="stylesheet" type="text/css" href="assets/normalize.css">
+      <!--Machinewrite-->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+      <!-- Global site tag (gtag.js) - Google Pichuytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-74004200-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-74004200-1');
+        </script>
+       <!-- Global site tag (gtag.js) - Google Pichuytics -->
+
+
+</head>
+
+<body class="my-body" data-spy="scroll" data-target="#main-navbar" data-offset="70">
+
+<!--barraDeProgreso-->
+<div class="headere">
+  <div class="progress-container">
+    <div class="progress-bar" id="myBar"></div>
+  </div>  
+</div>
+<!--barraDeProgreso-->  
+<div class="row" align="left">
+  <div class="col m12 s12">
+
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?5eb2dhLOYkK9hUr7TrwizgJSyPxysQlv";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
+  <!-- {/literal} END JIVOSITE CODE -->
+  </div>
+</div>
+<!--menuluis-->
+
+<style type="text/css">
+.colormenu4{background-color: #4db6ac;}
+</style>
+
+<div class="fixed-action-btn vertical click-to-toggle">
+    <a class="btn-floating btn-large colormenu1 pulse" >
+      <i class="large material-icons">apps</i>
+    </a>
+    <ul>
+      <li><a href="#webfor" class="btn-floating colormenu4 tooltipped" data-position="left"  data-delay="50" data-tooltip="Contacto" data-activates="slide-out"><i class="material-icons">perm_phone_msg</i></a></li>
+       <li><a href="https://api.whatsapp.com/send?phone=51934244258&text=Hola%20Me%20Gustaria%20información%20de%20sus%20servicios" target="new" class="btn-floating colormenu4 tooltipped" data-position="left"  data-delay="50" data-tooltip="Escribenos a Whatsapp">
+        <img src="img/whatsapp.svg" width="27" style="color: white; padding-top: 6px;">
+      </a></li>
+      <li><a href="../blog.php" class="btn-floating colormenu4 tooltipped" data-position="left" data-delay="50" data-tooltip="Blog">   <i class="material-icons">developer_mode</i></a></li>
+      <li><a href="../proyectos/index.php" class="btn-floating colormenu4 tooltipped" data-position="left" data-delay="50" data-tooltip="Proyectos" href=""><i class="material-icons">brush</i></a></li>
+      <li><a href="../branding" class="btn-floating colormenu4 tooltipped" data-position="left" data-delay="50" data-tooltip="Branding"><i class="material-icons">format_color_fill</i></a></li>
+      <li><a href="../marketing-digital/index.php" class="btn-floating colormenu4 tooltipped" data-position="left" data-delay="50" data-tooltip="Marketing Digital"><i class="material-icons">touch_app</i></a></li>      
+      <li><a href="../index.php" class="btn-floating colormenu4 tooltipped" data-position="left"  data-delay="50" data-tooltip="Ir al Inicio">
+        <img src="img/home.svg" width="27" style="color: white; padding-top: 8px;">
+      </a></li> 
+
+    </ul>
+</div>
+<!--menuluis-->
+<!--slidervideo-->
+<style type="text/css">
+video{
+    position: fixed;
+   /* min-height: 100%;*/
+    min-width: 100%;
+    margin-top: 8px;
+    /*top: 50%;
+    left: 50%;*/
+    /*transform: translateX(-50%);
+    transform: translateY(-50%);*/
+    z-index: -1;}
+</style>
+<div class="sliders">
+    <video src="video-mano.mp4" autoplay loop></video>
+    <div class="container">
+      <div class="row" align="center">
+          <div class="col s12 line" >
+              <h1 class="animated fadeInLeft title_bn uptext">DISEÑO WEB</h1>
+              <h3 class="animated fadeInRight subtitle_bn uptext">QUE GENERA OPORTUNIDADES</h3>
+          </div>
+      </div>
+    </div>
+</div>
+<!--slidervideo-->
+
+<!--efectmachiwrite-->
+<div style="background-color: #53bcbd; margin-top: 0px;" align="center">
+  <div class="row" style="padding: 30px 30px 30px; margin-bottom: 0px;">
+  <h1 class="uptext">Desarrollamos <span class="gris" id="typed"></span></h1>
+  <h1 class="updateregular">Acorde a las últimas tendencias de desarrollo digital</h1> 
+        <script type="text/javascript">
+          $(function(){
+            $("#typed").typed({
+              strings: ["Magia Digital", "Webs que venden", "Webs con User Experience", "Tiendas virtuales con resultados"],
+              typeSpeed: 30,
+              backDelay: 1500,
+              loop: true,
+              loopCount: false,
+              callback: function(){ foo(); }
+            });
+            function foo(){ console.log("Callback"); }
+          });
+        </script>
+  </div>
+</div>
+<!--efectmachiwrite-->
+<!--infoItem-->
+<div class="container-fluid info_txt bg_info" align="center">
+    <h3 class="uptext item_title">Lo que conseguirás con tu diseño web:</h3>
+  <div class="container">
+    <div class="row" style="padding-bottom: 60px;">
+      <div class="col s12 m6 l3" align="center"> 
+        <span><img src="img/icon16161.png" class="item_img" ></span>
+        <h3 class="item_info uptext">DISEÑO COMERCIAL(UX)</h3>
+        <p>Diseños web que cumplen un Objetivo Comercial, ayudan a vender. (UX)</p>
+      </div>
+      <div class="col s12 m6 l3"> 
+        <span><img src="img/icon18406469.png" class="item_img" ></span>
+        <h3 class="item_info uptext">WEBS MULTIDISPOSITIVO</h3>
+        <p>Webs perfectamente visibles en dispositivos smartphones, tablets, pc. (Responsive)</p>
+      </div>
+      <div class="col s12 m6 l3"> 
+        <span><img src="img/icon5469333.png" class="item_img" ></span>
+        <h3 class="item_info uptext">GOOGLE OPTIMIZACIÓN</h3>
+        <p>Webs listas con palabras clave para ser encontradas en google (SEO).</p>
+      </div>
+      <div class="col s12 m6 l3"> 
+        <span><img src="img/icon16.png" class="item_img" ></span>
+        <h3 class="item_info uptext">AUTO ADMINISTRABLES</h3>
+        <p>Webs con gestión de contenidos. Administras tus fotos y texto (CMS).</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!--infoItem-->
+
+
+
+
+
+<div class="container-fluid" style="background-color: white; margin: 0px;">
+  <div class="container">
+    <div class="row" style="margin: 0px;" align="center"><br>
+
+        <h3 class="uptext item_title" style="margin-bottom: 10px;">Galeris de web.</h3>
+        <p>loren Ipsum moment text morat</p>
+
+
+        <div class="toolbar mb2 mt2">
+          <button class="btn fil-cat" href="" data-rel="all">Todas</button>
+          <button class="btn fil-cat" data-rel="web">loren #1</button>
+          <button class="btn fil-cat" data-rel="flyers">loren #2</button>
+          <button class="btn fil-cat" data-rel="bcards">loren #3</button>
+        </div> 
+         
+        <div id="portfolio">
+
+
+
+<div class="tile scale-anm web all">
+  <div class="capa">
+    <img src="http://via.placeholder.com/300x280" alt="Avatar" >
+    <div class="overlay">
+      <div class="text">Hello World</div>
+    </div>
+  </div>
+</div>
+
+
+<div class="tile scale-anm flyers all">
+  <div class="capa">
+    <img src="http://via.placeholder.com/300x300" alt="Avatar" >
+    <div class="overlay">
+      <div class="text">Hello World</div>
+    </div>
+  </div>
+</div>
+
+
+<div class="tile scale-anm bcards all">
+  <div class="capa">
+    <img src="http://via.placeholder.com/300x250" alt="Avatar" >
+    <div class="overlay">
+      <div class="text">Hello World</div>
+    </div>
+  </div>
+</div>
+
+
+<div class="tile scale-anm web all">
+  <div class="capa">
+    <img src="http://via.placeholder.com/300x350" alt="Avatar" >
+    <div class="overlay">
+      <div class="text">Hello World</div>
+    </div>
+  </div>
+</div>
+
+
+<div class="tile scale-anm flyers all">
+  <div class="capa">
+    <img src="http://via.placeholder.com/300x250" alt="Avatar" >
+    <div class="overlay">
+      <div class="text">Hello World</div>
+    </div>
+  </div>
+</div>
+
+
+<div class="tile scale-anm bcards all">
+  <div class="capa">
+    <img src="http://via.placeholder.com/300x320" alt="Avatar" >
+    <div class="overlay">
+      <div class="text">Hello World</div>
+    </div>
+  </div>
+</div> 
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<!--            <div class="tile scale-anm web all">
+                <img src="http://via.placeholder.com/300x250" alt="" />
+          </div>
+
+          <div class="tile scale-anm web all">
+                <img src="http://via.placeholder.com/300x320" alt="" />
+          </div>
+
+          <div class="tile scale-anm web all">
+                <img src="http://via.placeholder.com/300x400" alt="" />
+          </div>
+          <div class="tile scale-anm flyers all">
+                <img src="http://via.placeholder.com/300x300" alt="" />
+          </div>
+          <div class="tile scale-anm flyers all">
+                <img src="http://via.placeholder.com/300x250" alt="" />
+          </div>
+          <div class="tile scale-anm flyers all">
+                <img src="http://via.placeholder.com/300x300" alt="" />
+          </div>
+          <div class="tile scale-anm bcards all">
+                <img src="http://via.placeholder.com/300x250" alt="" />
+          </div>
+          <div class="tile scale-anm bcards all">
+                <img src="http://via.placeholder.com/300x300" alt="" />
+          </div>
+          <div class="tile scale-anm bcards all">
+                <img src="http://via.placeholder.com/300x400" alt="" />
+          </div>  -->
+
+
+
+
+        </div>
+
+        <div style="clear:both;"></div> 
+
+  
+    </div> 
+  </div>
+</div>
+
+
+
+<!-- cintillo-->
+    <div class="container-fluid bg_verde">
+      <div class="row" style="margin-bottom: 0px;">
+        <div class="col s12 m12 " style="padding: 35px;">
+          <h5 class="sd_sut2">¿Cómo puedo contactarlos?</h5>
+          <P class="sd_sut3">Comunícate ahora al <i class="fab fa-whatsapp"></i> 934244258 o bríndanos tus datos aquí</P>
+          <P class="sd_sut4">También puedes escribirnos a info@update.pe</P>
+        </div>
+      </div>
+    </div>
+<!-- cintillo-->
+<!--contaco-->
+    <div class="container-fluid" align="center" style="background-color: white;">
+      <div class="row" style="margin: 0;">
+        <div class="col s12 m8 l7 bg_contac" style="padding: 0px;"> 
+
+          <img src="img/a-9-decc-888x2.jpg" class="bg_contac responsive-img" style="height: auto;">
+          
+        </div>
+        <div class="col s12 m4 l5" id="contac"> 
+          <div class="container" id="fromta">
+           <div class="row" style="margin: 0px;">
+
+
+            <script id="bx24_form_inline" data-skip-moving="true">
+           (function(w,d,u,b){w['Bitrix24FormObject']=b;w[b] = w[b] || function(){arguments[0].ref=u;
+                (w[b].forms=w[b].forms||[]).push(arguments[0])};
+                if(w[b]['forms']) return;
+                var s=d.createElement('script');s.async=1;s.src=u+'?'+(1*new Date());
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+                })(window,document,'https://updatemarketing.bitrix24.es/bitrix/js/crm/form_loader.js','b24form');
+
+                b24form({"id":"17","lang":"la","sec":"2dz5dr","type":"inline"});
+            </script>
+
+           </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+<!--contaco-->
+<!--googleMaps-->
+    <div class="container-fluid bg_grey">
+      <div class="row ubicatex" style="margin-bottom:  0px;">
+        <div class="col s12 m12" style="padding: 50px;">
+          <h5 class="sd_suti_info2">Ubícanos</h5>
+          <P class="sd_sut4">Calle Martín de Murúa 150 - Edificio Empresarial PLEXUS<br>San Miguel, LIMA - PERÚ</P>
+        </div>
+      </div>
+    </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.38335528687!2d-77.09345382634368!3d-12.075590421389505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c96ff8023dc7%3A0xe2b0a2ebfb211589!2sPlexus+Centro+Empresarial!5e0!3m2!1ses-419!2spe!4v1525122313720" height="450" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
+<!--googleMaps-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <?php
+        include('../modulos/footer.php');
+        ?>
+
+
+
+<!--galeeria morrizon-->
+ <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+ <script type="text/javascript">
+   $(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+     $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+    
+  });
+});
+ </script>
+<!--galeeria morrizon-->
+
+
+
+
+        <!--Machinewrite-->
+        <script  src="js/index.js"></script>
+         <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+       
+        <!-- Bootstrap 4-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/materialize.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+
+
+          <script type="text/javascript">
+            $(document).ready(function() {
+                $('.scrollspy').scrollSpy();
+                $('.carousel').carousel();
+                $('.materialboxed').materialbox();
+                $('.modal').modal();
+                $('select').material_select();
+                $('.slider').slider({
+                  indicators: false,
+                  height: 600
+                });
+                $(".button-collapse").sideNav();
+
+              });
+
+          </script>
+        <script> 
+            // When the user scrolls the page, execute myFunction 
+            window.onscroll = function() {myFunction()};
+            function myFunction() {
+                var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+                var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+                var scrolled = (winScroll / height) * 100;
+                document.getElementById("myBar").style.width = scrolled + "%";
+                }
+        </script>
+
+
+</body>
+</html>
